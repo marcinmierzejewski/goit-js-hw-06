a3459a99de8be0c55f5090ec16ef02d358a0a109
Marcin Mierzejewski <mierzejewski2you@gmail.com>
2022-05-13 23:45:49 +0200

    finished task-02

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,12 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const list = document.querySelector("#ingredients");
+
+for (let i of ingredients) {
+  const firstElement = document.createElement("li");
+  firstElement.textContent = i;
+  firstElement.classList.add("item");
+  list.append(firstElement);
+}
