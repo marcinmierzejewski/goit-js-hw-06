a21efad4bf0e8887abc9b310b295c994eeb259d7
Marcin Mierzejewski <mierzejewski2you@gmail.com>
2022-05-14 23:26:19 +0200

    finished task-09

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,14 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const body = document.querySelector('body')
+const btnChange = document.querySelector(".change-color");
+const currentBgColor = document.querySelector(".color");
+
+const changeBgColor = () => {
+  body.style.backgroundColor = getRandomHexColor()
+  currentBgColor.textContent = body.style.backgroundColor;
+}
+
+btnChange.addEventListener("click", changeBgColor);
