e806b225871ec17ab5fde0cf32b902aa47f7a965
Marcin Mierzejewski <mierzejewski2you@gmail.com>
2022-05-14 22:52:33 +0200

    finished task-08

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,17 @@
+const loginForm = document.querySelector(".login-form");
 
+loginForm.addEventListener("submit", loginSubmit);
+
+function loginSubmit(e) {
+  e.preventDefault();
+  const {
+    elements: { email, password }
+  } = e.currentTarget;
+
+  if (email.value === "" || password.value === "") {
+    return alert("Email field and password cannot be empty!");
+  }
+
+  console.log(`Email: ${email.value}, Password: ${password.value}`);
+  e.currentTarget.reset();            
+}
