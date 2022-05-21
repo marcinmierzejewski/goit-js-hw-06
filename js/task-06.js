236ef1661687b3a1d1a3119bf9bf201f0ff17e5d
Marcin Mierzejewski <mierzejewski2you@gmail.com>
2022-05-21 10:46:33 +0200

    fixed task-06

--- js/task-06.js
+++ js/task-06.js
@@ -2,14 +2,12 @@ const textInput = document.querySelector("#validation-input");
 
 const checkInputText = () => {
   console.log("checking....");
-  if (textInput.value.length < Number(textInput.getAttribute("data-length"))) {
-    console.log("za mało znaków");
-    textInput.classList.remove("valid");
-    textInput.classList.add("invalid");
-  } else {
-    console.log("wszystko ok");
+  if (textInput.value.length == Number(textInput.getAttribute("data-length"))) {
     textInput.classList.remove("invalid");
     textInput.classList.add("valid");
+  } else {
+    textInput.classList.remove("valid");
+    textInput.classList.add("invalid");
   }
 };
 
