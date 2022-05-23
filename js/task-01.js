d900c1f822c3ea8990a0c0fda1aa885a75a18642
Marcin Mierzejewski <mierzejewski2you@gmail.com>
2022-05-23 22:48:46 +0200

    Update task-01.js

--- js/task-01.js
+++ js/task-01.js
@@ -32,3 +32,22 @@ console.log(
   Category: ${findH2[2].textContent}
   Elements: ${thirdListLi}`
 );
+
+
+// // Category list
+// const categories = document.querySelector("#categories");
+
+// // The categories list items
+// const items = document.querySelectorAll(".item");
+
+// // A function that returns the number of child elements
+// const numberOfChildren = (element) => element.children.length;
+
+// // Calculating the number of categories
+// console.log(`Number of categories: ${numberOfChildren(categories)}`);
+
+// // Find and enter a category name and the number of items in that category
+// items.forEach(i => {
+//   console.log(`Category: ${i.firstElementChild.textContent}`);
+//   console.log(`Elements: ${numberOfChildren(i.lastElementChild)}`);
+// });
